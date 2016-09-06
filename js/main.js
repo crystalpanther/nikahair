d01c2611fdfb807e71c67d37c163605035500ef4
andrey.ilutsa <andrey.ilutsa@gviagroup.com>
2016-09-06 19:35:19 +0300

    Added anchor buttons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 $(function() {
 
     $('.page-container').fullpage({
-        anchors: ['s1', 's2', 's3', 's4', 's5']
+        anchors: ['s1', 's2', 's3', 's4', 's5'],
+        navigation: true,
+        onLeave: function(index, nextIndex, direction) {
+            console.log(index, nextIndex, direction);
+            $('[data-anchor="s' + index + '"]').find('h2').removeClass('active');
+        },
+        afterLoad: function(anchorLink, index) {
+            console.log(anchorLink, index);
+            $('[data-anchor="' + anchorLink + '"]').find('h2').addClass('active');
+        }
+    });
+
+    $('[data-move]').click(function() {
+        var target = parseFloat($(this).attr('data-move'));
+        $.fn.fullpage.moveTo(target);
     });
 
 });
