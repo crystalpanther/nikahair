6e203daf9b5a0eeb2c24b69c26f5a79f1adc34dc
Elina <elina.lilova@gmail.com>
2016-09-20 18:23:12 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
+//here is fullPage plugin contoller
 $(function() {
-
     $('.page-container').fullpage({
-        anchors: ['s1', 's2', 's3', 's4', 's5'],
+        anchors: ['s1', 's2', 's3'],
         navigation: true,
-        onLeave: function(index, nextIndex, direction) {
-            console.log(index, nextIndex, direction);
-            $('[data-anchor="s' + index + '"]').find('h2').removeClass('active');
-        },
-        afterLoad: function(anchorLink, index) {
-            console.log(anchorLink, index);
-            $('[data-anchor="' + anchorLink + '"]').find('h2').addClass('active');
-        }
     });
-
     $('[data-move]').click(function() {
-        var target = parseFloat($(this).attr('data-move'));
-        $.fn.fullpage.moveTo(target);
-    });
+            var target = parseFloat($(this).attr('data-move'));
+            $.fn.fullpage.moveTo(target);
+        });
+});
+//here is mobile-menu controller
+$(function() {
+     $('.fa-bars').click(function() {
+         $('.mobile-page-menu').addClass("active");
 
+     });
+     $('.mobile-menu, .fa-times').click(function() {
+         $('.mobile-page-menu').removeClass("active");
+     });
 });
